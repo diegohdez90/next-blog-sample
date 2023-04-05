375f73d2958021d71c6ebec9c87bbc148c674a5b
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-04-05 12:46:54 -0600

    feat: add pages

---
+++ pages/posts/[...slug].js
@@ -0,0 +1,7 @@
+const Filter = () => {
+    return (<div>
+        <h1>Filter Posts</h1>
+    </div>)
+}
+
+export default Filter;
---
+++ pages/users/posts/index.js
@@ -0,0 +1,7 @@
+const UserPosts = () => {
+    return (<div>
+        <h1>User Posts</h1>
+    </div>)
+}
+
+export default Contact;
